76d42fac15b0841676dcb373335f4718c4c7ef63
kungfooman <lama12345@gmail.com>
2023-09-10 12:42:13 +0200

    Add parseJSDoc.mjs

---
+++ src/parseJSDoc.mjs
@@ -0,0 +1,80 @@
+import { expandTypeDepFree } from "./expandTypeDepFree.mjs";
+import { simplifyType } from "./simplifyType.mjs";
+
+/**
+ * @param {string} str - The type.
+ * @returns {string} The type with stripped default value.
+ */
+function stripDefaultValue(str) {
+  return str.split("=")[0];
+}
+
+/**
+ * @param {string} str - The type.
+ * @returns {boolean} Whether the type is optional or not.
+ */
+function isOptional(str) {
+  return str[0] === '[' && str[str.length - 1] === ']';
+}
+
+/**
+ * @param {string} str - The type.
+ * @returns {string}
+ */
+function stripOptional(str) {
+  if (isOptional(str)) {
+    str = str.slice(1, -1);
+  }
+  return str;
+}
+
+/**
+ * @param {string} src 
+ * @returns {{} | undefined}
+ */
+function parseJSDoc(src, expandType = expandTypeDepFree) {
+  const regex = /@param \{(.*?)\} ([\[\]a-zA-Z0-9_$=\.' ]+)/g;
+  const matches = [...src.matchAll(regex)];
+  /** @type {Record<string, any>} */
+  const params = {};
+  matches.forEach(_ => {
+    const type = expandType(_[1].trim());
+    let name = _[2].trim();
+    const optional = isOptional(name);
+    const simplifiedType = simplifyType(type, optional);
+    name = stripDefaultValue(stripOptional(name)).trim();
+    const parts = name.split(".");
+    if (parts.length == 3) { // Something like: @param {number[]} settings.render.skyboxRotation - Rotation of skybox.
+      let parts0 = parts[0]; // settings
+      let parts1 = parts[1]; // render
+      let parts2 = parts[2]; // skyboxRotation
+      const toptype = params[parts0];
+      toptype.properties[parts1].properties = toptype.properties[parts1].properties || {};
+      toptype.properties[parts1].properties[parts2] = simplifiedType;
+    } else if (parts.length == 2) { // Something like: @param {number} description[].components
+      let parts0 = parts[0]; // description[]
+      let parts1 = parts[1]; // components
+      if (parts0.endsWith('[]')) {
+        parts0 = parts0.slice(0, -2); // description[] -> description
+      }
+      const toptype = params[parts0];
+      if (toptype?.type == "union") {
+        const typeObject = toptype.members.find(_ => _?.type == 'object');
+        typeObject.properties[parts1] = simplifiedType;
+      } else if (toptype?.type == "array") {
+        toptype.elementType.properties[parts1] = simplifiedType;
+      } else {
+        toptype.properties = toptype.properties || {};
+        toptype.properties[parts1] = simplifiedType;
+      }
+    } else {
+      params[name] = simplifiedType;
+    }
+  });
+  if (Object.keys(params).length === 0) {
+    return;
+  }
+  return params;
+}
+
+export { parseJSDoc };
