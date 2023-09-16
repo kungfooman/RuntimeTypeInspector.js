6ba678b72277b5c704dbdaf406396011a87fcd05
kungfooman <lama12345@gmail.com>
2023-09-16 17:44:03 +0200

    Add ast2json.mjs

---
+++ src/ast2json.mjs
@@ -0,0 +1,13 @@
+/**
+ * @param {object} ast - The Babel AST.
+ * @returns {string} String representation in JSON format for debugging/inspecting the AST.
+ */
+function ast2json(ast) {
+  return JSON.stringify(ast, function(name, val) {
+    if (name == "loc" || name == "start" || name == "end") {
+      return undefined; // remove
+    }
+    return val; // keep
+  }, 2);
+}
+export { ast2json };
