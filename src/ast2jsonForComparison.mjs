5c3fbb715928cca14e4ce46ee2f7acdb872c499a
kungfooman <lama12345@gmail.com>
2023-09-22 16:52:51 +0200

    Add src/ast2jsonForComparison.mjs

---
+++ src/ast2jsonForComparison.mjs
@@ -0,0 +1,25 @@
+const drop = [
+  'loc',
+  'start',
+  'end',
+  'leadingComments',
+  'trailingComments',
+  'innerComments',
+  'innerComments',
+  'comments',
+];
+/**
+ * @example
+ * setRight(ast2jsonForComparison(parseSync("/** *"))); // Close comment with / after last *
+ * @param {object} ast - The Babel AST.
+ * @returns {string} String representation in JSON format for debugging/inspecting the AST.
+ */
+function ast2jsonForComparison(ast) {
+  return JSON.stringify(ast, function(name, val) {
+    if (drop.includes(name)) {
+      return undefined; // remove
+    }
+    return val; // keep
+  }, 2);
+}
+export {ast2jsonForComparison};
