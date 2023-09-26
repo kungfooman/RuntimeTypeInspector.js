1128915381d9367b9fb078474178ea1873fb98f1
kungfooman <lama12345@gmail.com>
2023-09-26 10:49:39 +0200

    add plugin-rollup/index.mjs

---
+++ plugin-rollup/index.mjs
@@ -0,0 +1,35 @@
+import { createFilter } from '@rollup/pluginutils';
+import { addTypeChecks } from '../src/addTypeChecks.mjs';
+import { expandType } from '../src/expandType.mjs';
+// todo getHeader() and only import what's required
+const header = "import { assertType, youCanAddABreakpointHere, validateDivision, registerTypedefs } from 'runtime-type-inspector';\n";
+/**
+ * @param {boolean} enable 
+ * @returns {import('rollup').Plugin}
+ */
+function runtimeTypeInspector(enable) {
+  console.log("FILTER ENABLED: ", enable);
+  const filter = createFilter([
+    '**/*.js'
+  ], []);
+  return {
+    name: 'runtime-type-inspector',
+    transform(code, id) {
+      if (!enable || !filter(id)) {
+        return undefined;
+      }
+      code = header + code;
+      // todo expose options to rollup plugin
+      code = addTypeChecks(code, {
+        validateDivision: false,
+        ignoreLocations: ['Tensor#constructor'], // todo
+        expandType,
+      });
+      return {
+        code,
+        map: null
+      };
+    }
+  };
+}
+export {runtimeTypeInspector};
