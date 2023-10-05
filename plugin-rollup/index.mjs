d09601442448f846a65b968e98076cbcacf2ade2
kungfooman <lama12345@gmail.com>
2023-10-05 12:54:58 +0200

    Refactor rollup plugin: add options for self-test and ignored files, because certain files are lifted into different contexts at runtime (e.g. into a WebWorker) and fail without access to e.g. validateDivision function

--- plugin-rollup/index.mjs
+++ plugin-rollup/index.mjs
@@ -1,6 +1,9 @@
-import {createFilter } from '@rollup/pluginutils';
-import {addTypeChecks} from '../src/addTypeChecks.mjs';
-import {expandType   } from '../src/expandType.mjs';
+import {createFilter         } from '@rollup/pluginutils';
+import {parseSync            } from '@babel/core';
+import {addTypeChecks        } from '../src/addTypeChecks.mjs';
+import {expandType           } from '../src/expandType.mjs';
+import {ast2jsonForComparison} from '../src/ast2jsonForComparison.mjs';
+import {code2ast2code        } from '../src/code2ast2code.mjs';
 /**
  * Alternatively "import * as rti from ..." would also prevent "Unused external imports" warning...
  * or keeping log of every single call during RTI parsing.
@@ -17,11 +20,29 @@ function getHeader(validateDivision) {
   return header;
 }
 /**
- * @param {boolean} enable 
+ * @param {string} left
+ * @param {string} right
+ */
+function compareAST(left, right) {
+  const l = parseSync(left);
+  const r = parseSync(right);
+  const ljson = ast2jsonForComparison(l);
+  const rjson = ast2jsonForComparison(r);
+  const test = ljson == rjson;
+  if (!test) {
+    console.warn(`AST is NOT equal`);
+  }
+}
+/**
+ * @param {object} [options] - Optional options.
+ * @param {boolean} [options.enable] - Enable or disable entire plugin. Defaults to true.
+ * @param {boolean} [options.selftest] - Every once in a while Babel changes the AST, so we
+ * self-test Stringifier class to ensure its functionanlity.
+ * @param {string[]} [options.ignoredFiles] - Ignore certain files which operate in a different
+ * context, for example framework/parsers/draco-worker.js operates as WebWorker (without RTI).
  * @returns {import('rollup').Plugin}
  */
-function runtimeTypeInspector(enable) {
-  console.log("FILTER ENABLED: ", enable);
+function runtimeTypeInspector({enable = true, selftest = false, ignoredFiles} = {}) {
   const filter = createFilter([
     '**/*.js'
   ], []);
@@ -30,6 +51,13 @@ function runtimeTypeInspector(enable) {
     transform(code, id) {
       if (!enable || !filter(id)) {
         return;
+      }
+      // Ignore files which are supposed to run in e.g. a Worker context without RTI
+      if (ignoredFiles?.some(_ => id.includes(_))) {
+        return;
+      }
+      if (selftest) {
+        compareAST(code, code2ast2code(code));
       }
       const validateDivision = true;
       code = getHeader(validateDivision) + code;
