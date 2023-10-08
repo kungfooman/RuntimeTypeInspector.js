4fb6aec5e96fc6a92f7abe08834f72b4b06ca9a7
kungfooman <lama12345@gmail.com>
2023-10-08 17:57:20 +0200

    simplify/extend parseJSDocTypedef and add test/jsdoc/typedef.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -6,6 +6,7 @@ export * from './expandType.mjs';
 export * from './expandTypeDepFree.mjs';
 export * from './parseJSDoc.mjs';
 export * from './parseJSDocSetter.mjs';
+export * from './parseJSDocTypedef.mjs';
 export * from './simplifyType.mjs';
 export * from './Stringifier.mjs';
 export * from './StringifierWithTypeAssertions.mjs';
--- src/parseJSDocTypedef.mjs
+++ src/parseJSDocTypedef.mjs
@@ -1,4 +1,26 @@
 /**
+ * @param {string} line 
+ * @returns {{content: string, nextIndex: number}}
+ */
+function extractCurlyContent(line) {
+  const firstCurly = line.indexOf('{');
+  let k = firstCurly + 1;
+  let count = 0;
+  for (; k<line.length; k++) {
+    const c = line[k];
+    if (c === '{') {
+      count++;
+    } else if (c === '}') {
+      count--;
+    }
+    if (count === -1) {
+      break;
+    }
+  }
+  const content = line.substring(firstCurly + 1, k);
+  return {content, nextIndex: k + 1};
+}
+/**
  * @param {Record<string, object>} typedefs 
  * @param {Console["warn"]} warn 
  * @param {import("@babel/types").CommentBlock | import("@babel/types").CommentLine} comment - From import("@babel/types").File["comments"]
@@ -11,47 +33,30 @@ function parseJSDocTypedef(typedefs, warn, comment, expandType) {
     return;
   }
   const lines = value.split('\n');
+  let lastTypedef = undefined;
   for (let line of lines) {
     line = line.trim();
     if (line[0] === '*') {
       line = line.slice(1).trim();
     }
     if (line.startsWith('@typedef')) {
-      const firstCurly = line.indexOf('{');
-      let k = firstCurly + 1;
-      let count = 0;
-      for (; k<line.length; k++) {
-        const c = line[k];
-        if (c === '{') {
-          count++;
-        } else if (c === '}') {
-          count--;
-        }
-        if (count === -1) {
-          break;
-        }
-      }
-      const def = line.substring(firstCurly + 1, k);
-      let i = k;
-      while (i < line.length) {
-        const c = line[i];
-        if (c === ' ') {
-          i++;
-          continue;
-        }
-        break;
-      }
-      if (line[i] !== '}') {
-        warn("expected }");
-        break;
-      }
-      i++;
-      let name = line.substring(i).trim();
+      const {content: def, nextIndex} = extractCurlyContent(line);
+      let name = line.substring(nextIndex).trim();
       // Drop description
       name = name.split(' ')[0];
-      typedefs[name] = expandType(def);
+      lastTypedef = expandTypeDepFree(def);
+      typedefs[name] = lastTypedef;
+    } else if (line.startsWith('@property')) {
+      const {content, nextIndex} = extractCurlyContent(line);
+      const rest = line.substring(nextIndex);
+      const propType = expandType(content);
+      if (lastTypedef?.type === 'object') {
+        lastTypedef.properties["todoName"] = propType;
+      } else {
+        warn("not an extensible type", lastTypedef);
+      }
     }
   }    
   return {};
 }
-export {parseJSDocTypedef};
+export {extractCurlyContent, parseJSDocTypedef};
