fdd4448aa43cfc4566888158f8a2e6cde7a6ce25
kungfooman <lama12345@gmail.com>
2023-10-14 16:54:20 +0200

    Remove false-positive from validateMap.mjs - still needs more work + tests

--- src-runtime/validateMap.mjs
+++ src-runtime/validateMap.mjs
@@ -1,6 +1,7 @@
 import {assertType   } from "./assertType.mjs";
 import {typecheckWarn} from "./typecheckWarn.mjs";
 /**
+ * @todo Implement checking all possible key/val types
  * @param {*} value 
  * @param {*} expect 
  * @param {*} loc 
@@ -14,10 +15,10 @@ function validateMap(value, expect, loc, name, critical) {
     typecheckWarn(`${loc}> validateType> map> unhandled key '${key}'`);
     return false;
   }
-  if (val !== 'any') {
-    typecheckWarn(`${loc}> validateType> map> expected any, not '${value}'`);
-    return false;
-  }
+  // if (val !== 'any') {
+  //   typecheckWarn(`${loc}> validateType> map> expected any, not '${value}'`);
+  //   return false;
+  // }
   let ret = true;
   for (const [k, v] of value) {
     const good = assertType(
