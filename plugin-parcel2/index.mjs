117e0c483fa63e1c2b777a0f28ee8e48f087ef22
kungfooman <lama12345@gmail.com>
2023-10-26 17:11:10 +0200

    plugin-parcel2: implement and publish as @runtime-type-inspector/parcel-transformer

--- plugin-parcel2/index.mjs
+++ plugin-parcel2/index.mjs
@@ -1,4 +1,18 @@
-function runtimeTypeInspector({enable = true, selftest = false, ignoredFiles} = {}) {
-    throw "unimplemented";
-}
-export {runtimeTypeInspector};
+import {Transformer} from '@parcel/plugin';
+import {addTypeChecks} from 'runtime-type-inspector';
+export default new Transformer({
+  async transform({asset}) {
+    // Retrieve the asset's source code and source map.
+    let source = await asset.getCode();
+    //let sourceMap = await asset.getMap();
+    // Run it through some compiler, and set the results 
+    // on the asset.
+    //let {code, map} = compile(source, sourceMap);
+    //asset.setCode(code);
+    //asset.setMap(map);
+    const code = addTypeChecks(source);
+    asset.setCode(code);
+    // Return the asset
+    return [asset];
+  }
+});
