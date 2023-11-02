9a8b9664d7eef25947d7d44f267c45c53094e95f
kungfooman <lama12345@gmail.com>
2023-11-02 12:46:58 +0100

    First draft of repl/test.html, making it quicker to work with every test we have to check for regressions Also fix by renaming mixed up input/output of simple-ObjectMethod-computed-*.mjs

---
+++ repl/test.js
@@ -0,0 +1,63 @@
+/**
+ * @param {string} a 
+ * @param {string} b 
+ * @returns 
+ */
+function addDiff(a, b) {
+  const element = document.createElement('div');
+  document.getElementById("here").innerHTML = '';
+  document.getElementById("here").append(element);
+  return new AceDiff({
+    element,
+    left: {
+      content: a,
+    },
+    right: {
+      content: b,
+    },
+  });
+}
+async function main() {
+  const resp = await fetch("../test/typechecking.json");
+  const json = await resp.json();
+  if (false)
+  for (const {input, output} of json) {
+    const res       = await fetch('../' + input);
+    const txtInput  = await res.text();
+    const res2      = await fetch('../' + output);
+    const txtOutput = await res2.text();
+    const div = document.createElement('div');
+    document.body.append(div);
+    const aceDiffer = new AceDiff({
+      //element: '.custom',
+      element: div,
+      left: {
+        content: txtInput,
+      },
+      right: {
+        content: txtOutput,
+      },
+    });
+  }
+  const buttonsDiv = document.getElementById('buttons');
+  const aceDiffer = addDiff('1\n2\n3\n4\n', '1\n2\n33\n4\n');
+  for (const {input, output} of json) {
+    const button = document.createElement('button');
+    buttonsDiv.append(button);
+    console.log("add")
+    button.innerText = input;
+    button.onclick = async () => {
+      const res       = await fetch('../' + input);
+      const txtInput  = await res.text();
+      const res2      = await fetch('../' + output);
+      const txtOutput = await res2.text();
+      aceDiffer.editors.left.ace.setValue(txtInput);
+      aceDiffer.editors.left.ace.clearSelection();
+      aceDiffer.editors.right.ace.setValue(txtOutput);
+      aceDiffer.editors.right.ace.clearSelection();
+      //console.log({input, output});
+    }
+  }
+  Object.assign(window, {aceDiffer, json});
+}
+main();
--- test/typechecking/simple-ObjectMethod-computed-output.mjs
+++ test/typechecking/simple-ObjectMethod-computed-output.mjs
@@ -1,9 +1,10 @@
 function VecAxis(axis = 'x', value = 0) {
+  // TODO: one indentation level too much
   return {
-    x: 0,
-    y: 0,
-    z: 0,
-    [axis]: value
+      x: 0,
+      y: 0,
+      z: 0,
+      [axis]: value
   };
 }
 const vec = VecAxis('y', 10);
