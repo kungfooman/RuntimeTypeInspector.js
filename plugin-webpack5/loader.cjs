e24d615e37f90bbf094c07fe0e1c6a55fb7b2df3
kungfooman <lama12345@gmail.com>
2023-11-19 17:30:48 +0100

    loader.cjs: fix type

--- plugin-webpack5/loader.cjs
+++ plugin-webpack5/loader.cjs
@@ -4,7 +4,7 @@ const loaderUtils = require('loader-utils');
  * @param {string} source - The source.
  */
 module.exports = function (source) {
-  /** @type {import('@runtime-type-inspector/transpiler'.Options)} */
+  /** @type {import('@runtime-type-inspector/transpiler').Options} */
   const options = loaderUtils.getOptions(this);
   source = addTypeChecks(source, options);
   this.callback(null, source);
