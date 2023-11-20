7f413a543ebe78972dbb2cbc88539da33c72bfca
kungfooman <lama12345@gmail.com>
2023-11-20 15:37:51 +0100

    just some quick linting

--- test/typechecking/todo-restelement.mjs
+++ test/typechecking/todo-restelement.mjs
@@ -1,21 +1,21 @@
 /**
  * Joins multiple parts of a path into a single path, while handling leading and trailing slashes.
  *
- * @param {...string} parts Multiple parts of a path.
+ * @param {...string} parts - Multiple parts of a path.
  * @returns {string} A string representing the joined path.
  */
 function pathJoin(...parts) {
-    // https://stackoverflow.com/a/55142565
-    parts = parts.map((part, index) => {
-        if (index) {
-            part = part.replace(new RegExp('^/'), '');
-        }
-        if (index !== parts.length - 1) {
-            part = part.replace(new RegExp('/$'), '');
-        }
-        return part;
-    })
-    return parts.join('/');
+  // https://stackoverflow.com/a/55142565
+  parts = parts.map((part, index) => {
+    if (index) {
+      part = part.replace(new RegExp('^/'), '');
+    }
+    if (index !== parts.length - 1) {
+      part = part.replace(new RegExp('/$'), '');
+    }
+    return part;
+  });
+  return parts.join('/');
 }
 const ret = pathJoin('/etc', 'passwd');
 console.log('ret', ret);
