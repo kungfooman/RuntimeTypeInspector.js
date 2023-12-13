4ab7f3bf6c4e7c3a38b9a9d916490921cb34e080
kungfooman <lama12345@gmail.com>
2023-12-13 17:11:04 +0100

    fix linting and importmap

--- repl/test.js
+++ repl/test.js
@@ -1,7 +1,7 @@
 /**
- * @param {string} a 
- * @param {string} b 
- * @returns 
+ * @param {string} a - Left text.
+ * @param {string} b - Right text.
+ * @returns {object} AceDiff editor instance.
  */
 function addDiff(a, b) {
   const element = document.createElement('div');
@@ -20,31 +20,33 @@ function addDiff(a, b) {
 async function main() {
   const resp = await fetch("../test/typechecking.json");
   const json = await resp.json();
-  if (false)
-  for (const {input, output} of json) {
-    const res       = await fetch('../' + input);
-    const txtInput  = await res.text();
-    const res2      = await fetch('../' + output);
-    const txtOutput = await res2.text();
-    const div = document.createElement('div');
-    document.body.append(div);
-    const aceDiffer = new AceDiff({
-      //element: '.custom',
-      element: div,
-      left: {
-        content: txtInput,
-      },
-      right: {
-        content: txtOutput,
-      },
-    });
-  }
+  // for (const {input, output} of json) {
+  //   const res       = await fetch('../' + input);
+  //   const txtInput  = await res.text();
+  //   const res2      = await fetch('../' + output);
+  //   const txtOutput = await res2.text();
+  //   const div = document.createElement('div');
+  //   document.body.append(div);
+  //   const aceDiffer = new AceDiff({
+  //     //element: '.custom',
+  //     element: div,
+  //     left: {
+  //       content: txtInput,
+  //     },
+  //     right: {
+  //       content: txtOutput,
+  //     },
+  //   });
+  // }
   const buttonsDiv = document.getElementById('buttons');
+  if (!(buttonsDiv instanceof HTMLDivElement)) {
+    throw new Error("buttons isn't a <div>");
+  }
   const aceDiffer = addDiff('1\n2\n3\n4\n', '1\n2\n33\n4\n');
   for (const {input, output} of json) {
     const button = document.createElement('button');
     buttonsDiv.append(button);
-    console.log("add")
+    console.log("add", button);
     button.innerText = input;
     button.onclick = async () => {
       const res       = await fetch('../' + input);
@@ -56,7 +58,7 @@ async function main() {
       aceDiffer.editors.right.ace.setValue(txtOutput);
       aceDiffer.editors.right.ace.clearSelection();
       //console.log({input, output});
-    }
+    };
   }
   Object.assign(window, {aceDiffer, json});
 }
