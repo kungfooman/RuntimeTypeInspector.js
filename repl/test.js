5c7a8d1ca0f1719a2d568da85379d06ab5be42a2
kungfooman <lama12345@gmail.com>
2023-12-13 17:37:07 +0100

    Running full test suite in browser and visual feedback

--- repl/test.js
+++ repl/test.js
@@ -1,3 +1,4 @@
+import {addTypeChecks, expandType} from "@runtime-type-inspector/transpiler";
 /**
  * @param {string} a - Left text.
  * @param {string} b - Right text.
@@ -20,46 +21,42 @@ function addDiff(a, b) {
 async function main() {
   const resp = await fetch("../test/typechecking.json");
   const json = await resp.json();
-  // for (const {input, output} of json) {
-  //   const res       = await fetch('../' + input);
-  //   const txtInput  = await res.text();
-  //   const res2      = await fetch('../' + output);
-  //   const txtOutput = await res2.text();
-  //   const div = document.createElement('div');
-  //   document.body.append(div);
-  //   const aceDiffer = new AceDiff({
-  //     //element: '.custom',
-  //     element: div,
-  //     left: {
-  //       content: txtInput,
-  //     },
-  //     right: {
-  //       content: txtOutput,
-  //     },
-  //   });
-  // }
   const buttonsDiv = document.getElementById('buttons');
   if (!(buttonsDiv instanceof HTMLDivElement)) {
     throw new Error("buttons isn't a <div>");
   }
   const aceDiffer = addDiff('1\n2\n3\n4\n', '1\n2\n33\n4\n');
+  /** @type {HTMLButtonElement[]} */
+  const buttons = [];
   for (const {input, output} of json) {
     const button = document.createElement('button');
     buttonsDiv.append(button);
-    console.log("add", button);
     button.innerText = input;
+    const res       = await fetch('../' + input);
+    const txtInput  = await res.text();
+    const res2      = await fetch('../' + output);
+    const txtOutput = await res2.text();
+    let actualResult = addTypeChecks(txtInput, {expandType, addHeader: false});
+    // Remove multiple newlines into one
+    // I would rather not do it, but Stringifier needs a bit more love in other areas:
+    //  - multiline array output when elements surpass a max-col option
+    actualResult = actualResult.replace(/\n+/g, '\n').trim();
+    const success = actualResult === txtOutput.trim();
+    button.style.backgroundColor = success ? 'lime' : 'red';
     button.onclick = async () => {
-      const res       = await fetch('../' + input);
-      const txtInput  = await res.text();
-      const res2      = await fetch('../' + output);
-      const txtOutput = await res2.text();
       aceDiffer.editors.left.ace.setValue(txtInput);
       aceDiffer.editors.left.ace.clearSelection();
-      aceDiffer.editors.right.ace.setValue(txtOutput);
+      let rightText = txtOutput;
+      if (!success) {
+        rightText += `\n\n// Actual result:\n${actualResult}`;
+      }
+      aceDiffer.editors.right.ace.setValue(rightText);
       aceDiffer.editors.right.ace.clearSelection();
       //console.log({input, output});
     };
+    buttons.push(button);
   }
+  buttons[1].onclick();
   Object.assign(window, {aceDiffer, json});
 }
 main();
