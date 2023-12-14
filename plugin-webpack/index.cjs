bd368189a62ecb787c1346ec8267248d4e23d8f1
kungfooman <lama12345@gmail.com>
2023-12-14 17:58:01 +0100

    Implement plugin-webpack/index.cjs

--- plugin-webpack/index.cjs
+++ plugin-webpack/index.cjs
@@ -1,4 +1,6 @@
-function runtimeTypeInspector({enable = true, selftest = false, ignoredFiles} = {}) {
-    throw "unimplemented";
+const {version} = require('webpack');
+if (parseInt(version) <= 4) {
+  module.exports = require('@runtime-type-inspector/plugin-webpack4');
+} else {
+  module.exports = require('@runtime-type-inspector/plugin-webpack5');
 }
-export {runtimeTypeInspector};
