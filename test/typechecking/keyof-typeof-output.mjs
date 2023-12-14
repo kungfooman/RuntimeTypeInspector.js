52ff9322ad23134912d20e2f73caa463ebd3adca
kungfooman <lama12345@gmail.com>
2023-12-14 12:57:49 +0100

    add unit test

---
+++ test/typechecking/keyof-typeof-output.mjs
@@ -0,0 +1,40 @@
+registerTypedef('ObjKey', {
+  "type": "keyof",
+  "argument": {
+    "type": "typeof",
+    "argument": "obj"
+  }
+});
+registerTypedef('DataType', {
+  "type": "keyof",
+  "argument": {
+    "type": "typeof",
+    "argument": "DataTypeMap"
+  }
+});
+/** @todo keep in one line */
+const obj = {
+  a: 1,
+  b: 2,
+  c: 3
+};
+/**
+ * @typedef {keyof typeof obj} ObjKey - Will be: "a" | "b" | "c"
+ */
+const DataTypeMap = Object.freeze({
+  float32: Float32Array,
+  float64: Float64Array,
+  string: Array,
+  int8: Int8Array,
+  uint8: Uint8Array,
+  int16: Int16Array,
+  uint16: Uint16Array,
+  int32: Int32Array,
+  uint32: Uint32Array,
+  int64: BigInt64Array,
+  uint64: BigUint64Array,
+  bool: Uint8Array,
+});
+/**
+ * @typedef {keyof typeof DataTypeMap} DataType
+ */
