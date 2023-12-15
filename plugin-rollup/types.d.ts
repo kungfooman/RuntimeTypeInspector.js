2936d74ee811379478f147c9b5852f2c4f27412e
kungfooman <lama12345@gmail.com>
2023-12-15 16:27:38 +0100

    Fix types for plugin-rollup

--- plugin-rollup/types.d.ts
+++
@@ -1,4 +0,0 @@
-// The content of this types.d.ts file is authored manually. 
-type Options = import('./index.mjs').Options;
-declare function runtimeTypeInspector(options?: Options): import('rollup').Plugin;
-export {runtimeTypeInspector};
