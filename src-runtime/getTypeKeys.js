c90558cb2818f80546a20ff508eb372fabaca87d
kungfooman <lama12345@gmail.com>
2024-01-10 18:01:04 +0100

    getTypeKeys: work out more type cases

--- src-runtime/getTypeKeys.js
+++ src-runtime/getTypeKeys.js
@@ -11,7 +11,18 @@ import {typedefs } from "./registerTypedef.mjs";
  * @returns {string[]|undefined} Keys or undefined.
  */
 function getTypeKeys(expect, warn) {
-  if (expect?.type === 'typeof') {
+  if (typeof expect === 'string') {
+    if (typedefs[expect]) {
+      const typedef = typedefs[expect];
+      return getTypeKeys(typedef, warn);
+      //warn("getTypeKeys: Unhandled typedef", {expect, typedef});
+      //return;
+    }
+    warn("getTypeKeys> 'expect' was a string but not a typedef, unhandled case.");
+    return;
+  }
+  const {type} = expect;
+  if (type === 'typeof') {
     const varName = expect.argument;
     const variable = variables[varName];
     if (variable === undefined) {
@@ -19,12 +30,14 @@ function getTypeKeys(expect, warn) {
       return;
     }
     return Object.keys(variable);
-  } else if (typedefs[expect]) {
-    const typedef = typedefs[expect];
-    if (typedef.type === 'union') {
-      return typedef.members;
-    }
-    warn("getTypeKeys: Unhandled typedef", {expect, typedef});
+  } else if (type === 'union') {
+    return expect.members;
+  } else if (type === 'object') {
+    return Object.keys(expect.properties);
+  } else if (type === 'keyof') {
+    const {argument} = expect;
+    //console.log("want key for", argument);
+    return getTypeKeys(argument, warn);
   }
   warn(`Couldn't get keys for type`, expect);
 }
