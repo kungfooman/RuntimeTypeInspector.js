af8daff1a03853dbdba8714d41005eae02ad6fb4
kungfooman <lama12345@gmail.com>
2024-01-11 16:10:04 +0100

    replaceType: add tuple support

--- src-runtime/replaceType.js
+++ src-runtime/replaceType.js
@@ -3,22 +3,41 @@
  *  - To prevent circularity, check that "search" isn't in "replace" in the first place?
  *  - add depth argument
  *  - add bunch of unit tests to test all possible cases
+ *  - call it copyAndReplace?
+ *  - if a subtype is a typedef and we change that without creating a copy, we invalidate that typedef... make a unit test
  * @param {*} type - The type.
  * @param {*} search - The search.
  * @param {*} replace - The replace.
  * @param {*} warn - The warn.
+ * @returns {any} - A copy of the structured type.
  */
 function replaceType(type, search, replace, warn) {
+  //if (typeof type )
+  if (type === search) {
+    // console.log("replaceType", {type, search, replace, warn});
+    return replace;
+  }
   if (type.type === 'object') {
     const {properties} = type;
+    // todo I need unit tests making sure I don't need a full copy
+    // const newProperties = {};
     for (const prop in properties) {
       const val = properties[prop];
-      if (val === search) {
-        // console.log("replaceType", {prop, properties, search, replace});
-        properties[prop] = replace;
-      }
+      const replacedVal = replaceType(val, search, replace, warn);
+      properties[prop] = replacedVal;
+      properties[prop] = val;
     }
-    return;
+    return type;
+  } else if (type.type === "tuple") {
+    const {elements} = type;
+    const n = elements.length;
+    for (let i = 0; i < n; i++) {
+      const val = elements[i];
+      const replacedVal = replaceType(val, search, replace, warn);
+      elements[i] = replacedVal;
+      //if ()
+    }
+    return type;
   }
   warn('replaceType: unhandled', {type, search, replace});
 }
