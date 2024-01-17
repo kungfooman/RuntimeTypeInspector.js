105e5a15dc34d018fd603eb320d79556f01a61c0
kungfooman <lama12345@gmail.com>
2024-01-17 11:52:01 +0100

    Add src-runtime/validateIndexedAccess.js (only for `IndexedAccess` typedef right now)

---
+++ src-runtime/validateIndexedAccess.js
@@ -0,0 +1,6 @@
+/**
+ * @typedef {object} IndexedAccess
+ * @property {'indexedAccess'} type - The type.
+ * @property {import('./validateType.mjs').Type} object - The object, for example `Obj`
+ * @property {import('./validateType.mjs').Type} index - The index, for example `{type: 'keyof', argument: 'Obj'}`
+ */
