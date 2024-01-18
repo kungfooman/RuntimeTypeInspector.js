c5ff83e46fdf77f26fa2e0eb55fcbe7de9cf5539
kungfooman <lama12345@gmail.com>
2024-01-18 12:01:06 +0100

    add TypePanel class

--- src-runtime/createDiv.mjs
+++ src-runtime/createDiv.mjs
@@ -24,80 +24,86 @@ function niceDiv(div) {
   div.classList.add('rti');
   document.head.appendChild(rule);
 }
+class TypePanel {
+  div             = document.createElement('div');
+  spanErrors      = document.createElement('span');
+  span            = document.createElement('span');
+  select          = document.createElement('select');
+  option_spam     = document.createElement('option');
+  option_once     = document.createElement('option');
+  option_never    = document.createElement('option');
+  buttonHide      = document.createElement('button');
+  buttonSaveState = document.createElement('button');
+  constructor() {
+    const {div, spanErrors, span, select, option_spam, option_once, option_never, buttonHide, buttonSaveState} = this;
+    div.style.position = "absolute";
+    div.style.bottom = "0px";
+    div.style.right = "0px";
+    div.style.zIndex = "10";
+    niceDiv(div);
+    span.innerText = " Type report mode:";
+    option_spam.text = 'spam';
+    option_once.text = 'once';
+    option_never.text = 'never';
+    select.append(option_spam, option_once, option_never);
+    const spamTypeReports = localStorage.getItem('rti-spam-type-reports');
+    select.value = options.mode;
+    if (spamTypeReports !== null) {
+      select.value = spamTypeReports;
+    }
+    const onchange = () => {
+      const {value} = select;
+      localStorage.setItem('rti-spam-type-reports', value);
+      assertMode(value);
+      options.mode = value;
+    };
+    select.onchange = onchange;
+    onchange(); // set mode in options
+    buttonHide.textContent = 'Hide';
+    buttonHide.onclick = () => {
+      div.style.display = 'none';
+    };
+    buttonSaveState.textContent = 'Save state';
+    buttonSaveState.onclick = () => {
+      console.log("Save state ");
+      /** @type {object[]} */
+      const fullState = [];
+      /**
+       * @todo I would rather save loc/name because it's less likely to change in future... to keep state URL's alive
+       */
+      for (const key in options.warned) {
+        const e = options.warned[key];
+        const {deltaState} = e;
+        if (deltaState) {
+          const {loc, name} = e;
+          fullState.push({loc, name, ...deltaState});
+        }
+      }
+      console.log("fullState", JSON.stringify(fullState), fullState);
+      // location.hash =
+    };
+    div.append(spanErrors, span, select, buttonHide, buttonSaveState, warnedTable);
+    div.style.maxHeight = '200px';
+    div.style.overflow = 'scroll';
+    const finalFunc = () => document.body.append(div);
+    // Add our <div> to <body> when possible
+    if (document.readyState === "complete") {
+      finalFunc();
+    } else {
+      // add when page is loaded
+      document.addEventListener("DOMContentLoaded", finalFunc);
+    }
+    /** @todo debounce or get rid of this interval, ugly html changes all the time even if not needed */
+    setInterval(() => {
+      spanErrors.innerText = `Type validation errors: ${options.count}`;
+    }, 100);
+  }
+}
 /**
  * @returns {HTMLDivElement} The <div> at bottom/right position.
  */
 function createDiv() {
-  const div = document.createElement("div");
-  div.style.position = "absolute";
-  div.style.bottom = "0px";
-  div.style.right = "0px";
-  div.style.zIndex = "10";
-  niceDiv(div);
-  const spanErrors = document.createElement("span");
-  const span = document.createElement("span");
-  span.innerText = " Type report mode:";
-  const select = document.createElement("select");
-  const option_spam = document.createElement('option');
-  option_spam.text = 'spam';
-  const option_once = document.createElement('option');
-  option_once.text = 'once';
-  const option_never = document.createElement('option');
-  option_never.text = 'never';
-  select.append(option_spam, option_once, option_never);
-  const spamTypeReports = localStorage.getItem('rti-spam-type-reports');
-  select.value = options.mode;
-  if (spamTypeReports !== null) {
-    select.value = spamTypeReports;
-  }
-  const onchange = () => {
-    const {value} = select;
-    localStorage.setItem('rti-spam-type-reports', value);
-    assertMode(value);
-    options.mode = value;
-  };
-  select.onchange = onchange;
-  onchange(); // set mode in options
-  const buttonHide = document.createElement("button");
-  buttonHide.textContent = 'Hide';
-  buttonHide.onclick = () => {
-    div.style.display = 'none';
-  };
-  const buttonSaveState = document.createElement("button");
-  buttonSaveState.textContent = 'Save state';
-  buttonSaveState.onclick = () => {
-    console.log("Save state ");
-    /** @type {object[]} */
-    const fullState = [];
-    /**
-     * @todo I would rather save loc/name because it's less likely to change in future... to keep state URL's alive
-     */
-    for (const key in options.warned) {
-      const e = options.warned[key];
-      const {deltaState} = e;
-      if (deltaState) {
-        const {loc, name} = e;
-        fullState.push({loc, name, ...deltaState});
-      }
-    }
-    console.log("fullState", JSON.stringify(fullState), fullState);
-    // location.hash =
-  };
-  div.append(spanErrors, span, select, buttonHide, buttonSaveState, warnedTable);
-  div.style.maxHeight = '200px';
-  div.style.overflow = 'scroll';
-  const finalFunc = () => document.body.append(div);
-  // Add our <div> to <body> when possible
-  if (document.readyState === "complete") {
-    finalFunc();
-  } else {
-    // add when page is loaded
-    document.addEventListener("DOMContentLoaded", finalFunc);
-  }
-  /** @todo debounce or get rid of this interval, ugly html changes all the time even if not needed */
-  setInterval(() => {
-    spanErrors.innerText = `Type validation errors: ${options.count}`;
-  }, 100);
-  return div;
+  const typePanel = new TypePanel();
+  return typePanel.div;
 }
-export {createDiv};
+export {createDiv, TypePanel};
