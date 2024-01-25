37bf563c2c2974d7cde6a4b1dc3314b5ebecb433
kungfooman <lama12345@gmail.com>
2024-01-25 09:30:30 +0100

    Publish @runtime-type-inspector/runtime@3.2.0 Publish @runtime-type-inspector/transpiler@3.2.0 Publish @runtime-type-inspector/plugin-parcel1@3.2.0 Publish @runtime-type-inspector/parcel-transformer@3.2.0 Publish @runtime-type-inspector/plugin-rollup@3.2.0 Publish @runtime-type-inspector/plugin-webpack4@3.2.0 Publish @runtime-type-inspector/plugin-webpack5@3.2.0

--- npmhelper.js
+++ npmhelper.js
@@ -1,4 +1,4 @@
-import {readFileSync} from 'fs';
+import {readFileSync, writeFileSync} from 'fs';
 import {execSync} from 'child_process';
 /*
 cd plugin-parcel1 && npm publish
@@ -13,24 +13,45 @@ cd plugin-webpack5 && npm publish
 cd ..
 */
 const dirs = [
-    '@runtime-type-inspector/runtime',
-    '@runtime-type-inspector/transpiler',
-    'plugin-parcel1',
-    'plugin-parcel2',
-    'plugin-rollup',
-    'plugin-webpack4',
-    'plugin-webpack5',
+  '@runtime-type-inspector/runtime',
+  '@runtime-type-inspector/transpiler',
+  'plugin-parcel1',
+  'plugin-parcel2',
+  'plugin-rollup',
+  'plugin-webpack4',
+  'plugin-webpack5',
 ];
+const newVersion = '3.2.0';
 for (const dir of dirs) {
-    const file = `${dir}/package.json`;
-    const command = [
-        `cd ${dir}`,
-        'npm publish'
-    ].join(' && ');
-    //console.log(`# Package: ${dir}`);
-    //console.log(command);
-    //execSync(command);
-    const content = readFileSync(file, 'utf8');
-    const json = JSON.parse(content);
-    console.log(`Publish ${json.name}@${json.version}`);
+  const file = `${dir}/package.json`;
+  const command = [
+    `cd ${dir}`,
+    'npm publish'
+  ].join(' && ');
+  //console.log(`# Package: ${dir}`);
+  //console.log(command);
+  //execSync(command);
+  const content = readFileSync(file, 'utf8');
+  const json = JSON.parse(content);
+  json.version = newVersion;
+  for (const key in json.dependencies) {
+    if (key.includes('runtime-type-inspector')) {
+      json.dependencies[key] = `^${newVersion}`;
+    }
+  }
+  const newContent = JSON.stringify(json, null, 2) + '\n';
+  writeFileSync(file, newContent);
+}
+for (const dir of dirs) {
+  const command = [
+    `cd ${dir}`,
+    'npm publish',
+  ].join(' && ') + '\ncd ..';
+  console.log(command);
+}
+for (const dir of dirs) {
+  const file = `${dir}/package.json`;
+  const content = readFileSync(file, 'utf8');
+  const json = JSON.parse(content);
+  console.log(`Publish ${json.name}@${json.version}`);
 }
