3b46c290cace9b096cd0ecbbf535e500f198baa7
kungfooman <lama12345@gmail.com>
2024-02-18 11:35:37 +0100

    Extend test

--- test/typechecking/import-validation/b.js
+++ test/typechecking/import-validation/b.js
@@ -1,2 +1,10 @@
 const someNumber = 123;
-export {someNumber};
+function makeVec3Class() {
+    return function() {
+        this.x = 1;
+        this.y = 2;
+        this.z = 3;
+    }
+}
+const Vec3 = makeVec3Class();
+export {someNumber, makeVec3Class, Vec3};
