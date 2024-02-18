3fc1c87a4c649eb58b9b222d86e180fdebd11974
Hermann Rolfes <lama12345@gmail.com>
2024-02-18 11:41:33 +0100

    Validate import namespace functions by testing against prototype of value (#144)
    * Extend test
    
    * Check import-namespace-specifier against prototype of value
    
    * Comment console.log

--- test/typechecking/import-validation/a.js
+++ test/typechecking/import-validation/a.js
@@ -6,6 +6,20 @@ function test2(val) {
   console.log("test2 got", {val});
 }
 test2(123);
+/**
+ * @param {B.Vec3} vec - The vector.
+ */
+function testVec(vec) {
+  console.log("testVec got", {vec});
+}
+testVec(new B.Vec3());
+/**
+ * @param {B.makeVec3Class} vec - The vector.
+ */
+function testFunc(vec) {
+  console.log("testVec got", {vec});
+}
+testFunc(B.makeVec3Class);
 // /**
 //  * @param {typeof import('./b.js')} val 
 //  */
