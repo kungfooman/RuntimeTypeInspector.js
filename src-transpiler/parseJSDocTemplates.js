eefc14c7db0e95c61a1a6d3b39a32f35435ea96d
kungfooman <lama12345@gmail.com>
2024-05-19 16:03:53 +0200

    Add/implement parseJSDocTemplates.js

---
+++ src-transpiler/parseJSDocTemplates.js
@@ -0,0 +1,29 @@
+import {expandTypeDepFree} from "./expandTypeDepFree.js";
+/**
+ * @typedef {typeof expandTypeDepFree} ExpandTypeType
+ * @typedef {ReturnType<ExpandTypeType>} ExpandTypeReturnType
+ */
+/**
+ * Parses JSDoc comments to extract parameter type information.
+ *
+ * @param {string} src - The JSDoc comment string to parse.
+ * @param {ExpandTypeType} [expandType] - An optional function to process the types found in the JSDoc.
+ * @returns {Record<string, ExpandTypeReturnType> | undefined} An object mapping template names to their parsed types,
+ * or `undefined` if no template tags were found.
+ */
+function parseJSDocTemplates(src, expandType = expandTypeDepFree) {
+  const regexTemplateTyped = /@template \{(.*?)\} ([a-zA-Z0-9_$=]+)/g;
+  const matches = [...src.matchAll(regexTemplateTyped)];
+  if (!matches.length) {
+    return;
+  }
+  /** @type {Record<string, ExpandTypeReturnType>} */
+  const templates = Object.create(null);
+  matches.forEach(_ => {
+    const type = expandType(_[1].trim());
+    const name = _[2].trim();
+    templates[name] = type;
+  });
+  return templates;
+}
+export {parseJSDocTemplates};
