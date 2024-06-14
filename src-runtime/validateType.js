0b33c93ae5c321fafdea635ca8364054569defe5
kungfooman <lama12345@gmail.com>
2024-06-14 16:56:40 +0200

    Fix validateType, add comments

--- src-runtime/validateType.js
+++ src-runtime/validateType.js
@@ -172,10 +172,14 @@ function validateType(value, expect, loc, name, critical = true, warn, depth) {
     case 'IArguments':
       // Used in playcanvas-engine/src/core/tags.js
       // Testable via physics/offset-collision example.
+      /** @todo unit tests */
       return value[Symbol.iterator] instanceof Function;
     case 'ArrayBufferView':
+      /**
+       * @todo unit tests + TS Lib object like customObjects...
+       * for e.g. different TS versions or environments
+       */
       return ArrayBuffer.isView(value);
-    }
   }
   //if (value === null) {
   //  /** @todo Add unit-tests/asserts tests to make sure this never happens */
