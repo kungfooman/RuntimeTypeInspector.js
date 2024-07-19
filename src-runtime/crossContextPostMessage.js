4afdd561d91e48befe8441794e0e448e7ef7c362
kungfooman <lama12345@gmail.com>
2024-07-19 13:11:58 +0200

    crossContextPostMessage: fix lint

--- src-runtime/crossContextPostMessage.js
+++ src-runtime/crossContextPostMessage.js
@@ -2,7 +2,7 @@
  * Works to post messages from inside <iframe>'s and Workers to the parent window,
  * which should be the user interface that can handle/display the messages in an
  * integrated matter.
- * @param  {...any} args - The arguments.
+ * @param {...any} args - The arguments.
  */
 function crossContextPostMessage(...args) {
   const to = globalThis.parent || globalThis.self;
