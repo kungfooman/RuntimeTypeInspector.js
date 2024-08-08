6fb6768c4f371c2c918d00ec69c642b866fdddef
kungfooman <lama12345@gmail.com>
2024-08-08 16:26:50 +0200

    Fix lint

--- src-transpiler/capitalize.js
+++ src-transpiler/capitalize.js
@@ -1,8 +1,8 @@
 /**
  * @example
  * capitalize("hello"); // Outputs: Hello
- * @param {string} _ 
- * @returns {string}
+ * @param {string} _ - The input string.
+ * @returns {string} The capitalized output string.
  */
 function capitalize(_) {
   return _[0].toUpperCase() + _.slice(1);
