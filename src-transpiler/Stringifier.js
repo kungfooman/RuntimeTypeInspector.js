c32181735f506d80828ba6406f9f3691853a0d24
kungfooman <lama12345@gmail.com>
2024-11-04 17:41:57 +0100

    Fix ImportDeclaration

--- src-transpiler/Stringifier.js
+++ src-transpiler/Stringifier.js
@@ -1557,15 +1557,30 @@ class Stringifier {
     // ImportNamespaceSpecifier: import * as a   from "b";
     // ImportDefaultSpecifier  : import      a   from "b";
     // ImportSpecifier         : import {    a } from "b";
-    const a = this.mapToSource(specifiers).join(', ');
-    const b = this.toSource(source);
+    const from = this.toSource(source);
     if (specifiers.length === 0) {
-      return `import ${b}`;
+      return `import ${from}`;
     }
-    if (specifiers[0].type !== 'ImportSpecifier') {
-      return `import ${a} from ${b};`;
+    let out = 'import ';
+    const n = specifiers.length;
+    let inImportSpecifier = false;
+    let i = 0;
+    for (const specifier of specifiers) {
+      if (specifier.type === 'ImportSpecifier' && !inImportSpecifier) {
+        out += "{";
+        inImportSpecifier = true;
+      }
+      out += this.toSource(specifier);
+      if (i !== n - 1) {
+        out += ', ';
+      }
+      i++;
     }
-    return `import {${a}} from ${b};`;
+    if (inImportSpecifier) {
+      out += '}';
+    }
+    out += ` from ${from};`;
+    return out;
   }
   /**
    * @param {import("@babel/types").ImportSpecifier} node - The Babel AST node.
