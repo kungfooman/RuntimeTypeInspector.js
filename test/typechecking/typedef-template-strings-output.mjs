9a4857ea914130dd444b98e603cc9c43519971be
kungfooman <lama12345@gmail.com>
2026-09-12 13:17:58 +0200

    format

--- test/typechecking/typedef-template-strings-output.mjs
+++ test/typechecking/typedef-template-strings-output.mjs
@@ -1,50 +1,50 @@
 registerTypedef('EmailLocaleIDs', {
-"type": "union",
-"members": [
-"\"welcome_email\"",
-"\"email_heading\""
-]
+  "type": "union",
+  "members": [
+    "\"welcome_email\"",
+    "\"email_heading\""
+  ]
 });
 registerTypedef('FooterLocaleIDs', {
-"type": "union",
-"members": [
-"\"footer_title\"",
-"\"footer_sendoff\""
-]
+  "type": "union",
+  "members": [
+    "\"footer_title\"",
+    "\"footer_sendoff\""
+  ]
 });
 registerTypedef('AllLocaleIDs', {
-"type": "templateLiteral",
-"quasis": [
-"",
-"_id"
-],
-"types": [
-{
-"type": "union",
-"members": [
-"EmailLocaleIDs",
-"FooterLocaleIDs"
-]
-}
-]
+  "type": "templateLiteral",
+  "quasis": [
+    "",
+    "_id"
+  ],
+  "types": [
+    {
+      "type": "union",
+      "members": [
+        "EmailLocaleIDs",
+        "FooterLocaleIDs"
+      ]
+    }
+  ]
 });
 registerTypedef('Lang', {
-"type": "union",
-"members": [
-"\"en\"",
-"\"ja\"",
-"\"pt\""
-]
+  "type": "union",
+  "members": [
+    "\"en\"",
+    "\"ja\"",
+    "\"pt\""
+  ]
 });
 registerTypedef('LocaleMessageIDs', {
-"type": "templateLiteral",
-"quasis": [
-"",
-"_",
-""
-],
-"types": [
-"Lang",
-"AllLocaleIDs"
-]
+  "type": "templateLiteral",
+  "quasis": [
+    "",
+    "_",
+    ""
+  ],
+  "types": [
+    "Lang",
+    "AllLocaleIDs"
+  ]
 });
